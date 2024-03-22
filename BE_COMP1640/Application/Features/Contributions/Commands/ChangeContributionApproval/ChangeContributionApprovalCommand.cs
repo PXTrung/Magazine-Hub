13e12cd875ad6763fbb23174086fbf67e5b3ad06
Le Nguyen Khang <khanglngcs210650@fpt.edu.vn>
2024-03-22 16:25:52 +0700

    Implement endpoint for approve and reject contribution Update swagger comments

---
+++ BE_COMP1640/Application/Features/Contributions/Commands/ChangeContributionApproval/ChangeContributionApprovalCommand.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common.Models;
+using ErrorOr;
+using MediatR;
+
+namespace Application.Features.Contributions.Commands.ChangeContributionApproval
+{
+    public class ChangeContributionApprovalCommand : IRequest<ErrorOr<SuccessResult>>
+    {
+        public Guid Id { get; set; }
+
+        public bool Approved { get; set; }
+    }
+}
---
+++ BE_COMP1640/Application/Features/Contributions/Commands/ChangeContributionApproval/ChangeContributionApprovalCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Contributions.Commands.ChangeContributionApproval
+{
+    public class ChangeContributionApprovalCommandValidator : AbstractValidator<ChangeContributionApprovalCommand>
+    {
+        public ChangeContributionApprovalCommandValidator()
+        {
+            RuleFor(command => command.Id).NotEmpty().WithMessage("Id is required");
+
+            RuleFor(command => command.Approved).NotNull().WithMessage("Approved is required");
+        }
+    }
+}
