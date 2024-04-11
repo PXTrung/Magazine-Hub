e02e2abd2f6e3529cf71128ed944191a839a9a66
trung <trung@gmail.com>
2024-04-11 11:34:21 +0700

    fix

--- FE_COMP1640/src/pages/coordinator/components/ContributionDetail/FeedbackList.tsx
+++ FE_COMP1640/src/pages/coordinator/components/ContributionDetail/FeedbackList.tsx
@@ -22,16 +22,16 @@ const FeedbackList = (contribution: IFeedbackListProps) => {
   const allowedStatuses = ["Approved", "Rejected", "Published"];
   const thisPeriod = period.find((p) => p.id === contribution?.period);
 
-  const disableUpdate = () => {
-    if (allowedStatuses.includes(contribution.status as string)) {
-      return false;
-    }
+  // const disableUpdate = () => {
+  //   if (allowedStatuses.includes(contribution.status as string)) {
+  //     return false;
+  //   }
 
-    const today = new Date();
-    const deadline = new Date(thisPeriod?.secondSubmissionDeadline as string);
+  //   const today = new Date();
+  //   const deadline = new Date(thisPeriod?.secondSubmissionDeadline as string);
 
-    return today < deadline;
-  };
+  //   return today < deadline;
+  // };
 
   const handleOnchange = (e: any) => {
     setContent(e.target.value);
@@ -59,28 +59,24 @@ const FeedbackList = (contribution: IFeedbackListProps) => {
 
   return (
     <div className="ml-4 w-[400px] overflow-hidden">
-      {!disableUpdate() ? (
-        <div className="flex flex-col gap-2">
-          <div className="w-full flex flex-col gap-2 mb-1 border-b-2 border-gray-200 pb-4">
-            <input
-              type="text"
-              id="feedback"
-              placeholder="Your feedback"
-              className="w-full h-10 px-4 ring-0 outline-0"
-              onChange={(e) => handleOnchange(e)}
-              value={content}
-            />
-            <Button label="Submit" type="primary" onClick={submitFeedback} />
-          </div>
-          <div className="max-h-[400px] overflow-x-hidden overflow-y-scroll w-full flex flex-col gap-2 mb-4">
-            {feedback?.map((feedback) => {
-              return <Feedback feedback={feedback} key={feedback.id} />;
-            })}
-          </div>
+      <div className="flex flex-col gap-2">
+        <div className="w-full flex flex-col gap-2 mb-1 border-b-2 border-gray-200 pb-4">
+          <input
+            type="text"
+            id="feedback"
+            placeholder="Your feedback"
+            className="w-full h-10 px-4 ring-0 outline-0"
+            onChange={(e) => handleOnchange(e)}
+            value={content}
+          />
+          <Button label="Submit" type="primary" onClick={submitFeedback} />
         </div>
-      ) : (
-        "It is over second time line "
-      )}
+        <div className="max-h-[400px] overflow-x-hidden overflow-y-scroll w-full flex flex-col gap-2 mb-4">
+          {feedback?.map((feedback) => {
+            return <Feedback feedback={feedback} key={feedback.id} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
